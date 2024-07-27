438471ef966a5afa72bdfd31af268285656169b8
aZuh7 <tyler.lopez15@icloud.com>
2024-07-27 02:09:57 -0500

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,6 +3,8 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiURL = "/choreo-apis/djangoreact/backend/v1"
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
